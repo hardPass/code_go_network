710b840dc34e7af12dcfd7e9b207d7bad8c54303
hardPass <hardPass@126.com>
2013-03-19 11:34:40 +0800

    Create exhaustive.go

---
+++ exhaustive.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+	count := 0
+	a := [4]string{}
+	for i := 0; i < 4; i++ {
+		a[i] = "A"
+		for j := 0; j < 4; j++ {
+			if i == j {
+				continue
+			}
+			a[j] = "B"
+			for k := 0; k < 4; k++ {
+
+				if i == j || j == k || i == k {
+					continue
+				}
+				a[k] = "C"
+
+				for m := 0; m < 4; m++ {
+					if i == j || j == k || i == k || j == m || m == i || m == k {
+						continue
+					}
+					a[m] = "D"
+					count++
+					fmt.Println(count, a)
+				}
+			}
+		}
+	}
+	fmt.Println("Done.")
+}
