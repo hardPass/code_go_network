c5ca0e6332f63bbe43a67246605b66cd8c3b0c8b
hardPass <hardPass@126.com>
2013-06-03 10:46:44 +0800

    Update Chan_N_in_N_out.go

--- Chan_N_in_N_out.go
+++ Chan_N_in_N_out.go
@@ -2,63 +2,86 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"strconv"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
-var flag int64 = 0
+var (
+	flag int64 = 0
+	rw   sync.RWMutex
 
-var exitChan chan int = make(chan int)
+	c = make(chan string)
+)
 
-func ChnOut(n int, c <-chan string) {
+func Recv(n int) {
 	for {
-		data, ok := <-c
+		select {
+		case data, ok := <-c:
+			if !ok {
+				fmt.Println("Receiver ", n, " left.")
+				return
+			}
 
-		if !ok {
-			return
+			fmt.Println("Receiver ", n, " received:", data)
 		}
-
-		fmt.Println(n, "--", data)
-
+		//runtime.Gosched()
 	}
 
 }
 
-func ChnIn(n int, c chan<- string) {
-	for i := 0; i < 20; i++ {
+func Send(n int) {
+	for i := 0; ; i++ {
 		if atomic.LoadInt64(&flag) == 1 {
-			close(c)
+			fmt.Println("Sender ", n, " left.")
 			return
 		}
+		time.Sleep(time.Millisecond * 100)
+		// MustSend("<data " + strconv.Itoa(i) + "> from Sender" + strconv.Itoa(n))
 
-		data := strconv.Itoa(n) + "-" + strconv.Itoa(i)
-		c <- data
-
-		time.Sleep(time.Millisecond * 300)
+		c <- "<data " + strconv.Itoa(i) + "> from Sender" + strconv.Itoa(n)
 	}
 
+}
+
+func MustSend(data string) {
+	rw.RLock()
+	defer rw.RUnlock()
+
+	if atomic.LoadInt64(&flag) == 0 {
+		c <- data
+	}
+
+}
+
+func Close() {
+	rw.Lock()
+	defer rw.Unlock()
+
+	atomic.CompareAndSwapInt64(&flag, 0, 1)
+	close(c)
 }
 
 func main() {
+	runtime.GOMAXPROCS(4)
 	fmt.Println("Started...")
 
-	c := make(chan string, 10)
+	go Recv(1)
+	go Recv(2)
+	go Recv(3)
 
-	go ChnOut(1, c)
-	go ChnOut(2, c)
-	go ChnOut(3, c)
+	go Send(1)
+	go Send(2)
+	go Send(3)
 
-	go ChnIn(1, c)
-	go ChnIn(2, c)
-	go ChnIn(3, c)
-
-	time.Sleep(time.Millisecond * 2000)
+	time.Sleep(time.Millisecond * 1000)
 
 	fmt.Println("Prepare to done..")
-
-	atomic.CompareAndSwapInt64(&flag, 0, 1)
+	Close()
 
 	fmt.Println("Done!")
+	time.Sleep(1e10 * 2)
 
 }
